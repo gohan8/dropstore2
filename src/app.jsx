fb75f417de7014bda000b7f1cef8c16c3aa9e5bb
Paulo Souza <rsouza8@yandex.com>
2025-05-01 16:31:27 -0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProdcutList from './pages/ProdutcList';
+import ProductList from './pages/ProductList';
 import ProductPage from './pages/productPage';
 import CheckoutPage from './pages/checkoutPage';
 import DashboardPage from './pages/cashboardPage';
@@ -12,7 +12,7 @@ export default function App() {
       <Navbar />
       <div className="container mx-auto mt-6">
         <Routes>
-          <Route path="/" element={<ProdcutList />} />
+          <Route path="/" element={<ProductList />} />
           <Route path="/produto/:id" element={<ProductPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
