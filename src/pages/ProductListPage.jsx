330343d93b112f07e882969d286743a3f061c442
Paulo Souza <rsouza8@yandex.com>
2025-05-01 16:37:37 -0300

    Update and rename ProductList.jsx to ProductListPage.jsx

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,7 +1,7 @@
 // src/pages/productList.jsx
 import ProductList from '../components/ProductList';
 
-export default function ProductList() {
+export default function ProductListPage() {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold text-emerald-600 mb-4">Produtos Disponíveis</h1>
