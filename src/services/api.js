5b89d1008b24f8f3d8147512c86dad4ca3f4bbf2
Paulo Souza <rsouza8@yandex.com>
2025-05-01 22:08:07 -0300

    Update api.js. Correcting environment vars use.

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,21 @@
 // src/services/api.js
 import axios from 'axios';
-import dotenv from 'dotenv';
 
+dotenv.config();
 const api ={
   ali: axios.create({
-  baseURL: dotenv.env.ALI_API_BASE}),
-  mock: ()=> return [
+  baseURL: process.env.ALI_API_BASE}),
+  mock: ()=> {return [
     { nome: 'Product A',  preco: 100.00, descricao: `  This is a fake product for showing 
 something when the fetch to aliexpress api fails during development.
-  This should reoplce the standard description of the product that the online store is
+  This should replace the standard description of the product that the online store is
 selling.`},
     { nome: 'Product B', preco: 185.50, descricao: `  This another fake produto for placeholding
 the stores template for displaying products to the user.     
-  This is like the produc description. Like it is 3.4cm large and 2.8 cm tall with an area of   
+  This is like the product description. Like it is 3.4cm large and 2.8 cm tall with an area of   
 250 square meters.
   You will like it. Just BUY it. NOW!`}
-  ],
+  ]},
 }
 
 export default api;
