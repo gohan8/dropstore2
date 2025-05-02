ec4d5974e2ad15a53999bd25096366f6fd1394be
Paulo Souza <rsouza8@yandex.com>
2025-05-02 01:12:31 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
+import dotenv from 'dotenv'
 
+dotenv.config()
 if (!process.env.ALI_API_BASE) {
   throw new Error('ALI_API_BASE environment variable is not set');
 }
